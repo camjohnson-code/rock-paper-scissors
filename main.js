c8e8805c5994bdb920902e907347cc2181c4c58f
Cam Johnson <cameron.n.johnson15@gmail.com>
2023-11-09 12:15:36 -0700

    feature: Add functionality for selecting easy mode options

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var easyModePage = document.querySelector('.easy-mode');
 var hardModePage = document.querySelector('.hard-mode');
 var header = document.querySelector('header');
 var main = document.querySelector('main');
+var easyModeSection = document.querySelector('.easy-mode');
 var optionsSection = document.querySelector('.options');
 var easyModeRock = document.querySelector('.easy-mode-rock');
 var easyModePaper = document.querySelector('.easy-mode-paper');
@@ -13,9 +14,12 @@ var hardModePaper = document.querySelector('.hard-mode-paper');
 var hardModeScissors = document.querySelector('.hard-mode-scissors');
 var hardModeFire = document.querySelector('.hard-mode-fire');
 var hardModeWater = document.querySelector('.hard-mode-water');
-var easyRockIcons = easyModeRock.querySelectorAll('svg');
-var easyPaperIcons = easyModePaper.querySelectorAll('svg');
-var easyScissorsIcons = easyModeScissors.querySelectorAll('svg');
+var easyRockWhiteIcon = easyModeRock.querySelector('.white-svg');
+var easyRockSVGIcon = easyModeRock.querySelector('.gradient-svg');
+var easyPaperWhiteIcon = easyModePaper.querySelector('.white-svg');
+var easyPaperSVGIcon = easyModePaper.querySelector('.gradient-svg');
+var easyScissorsWhiteIcon = easyModeScissors.querySelector('.white-svg');
+var easyScissorsSVGIcon = easyModeScissors.querySelector('.gradient-svg');
 var hardRockIcons = hardModeRock.querySelectorAll('svg');
 var hardPaperIcons = hardModePaper.querySelectorAll('svg');
 var hardScissorsIcons = hardModeScissors.querySelectorAll('svg');
@@ -28,12 +32,11 @@ var homeBtn = document.querySelector('.home-button');
 var easyModeHeader = document.querySelector('.easy-mode-page');
 var hardModeHeader = document.querySelector('.hard-mode-page');
 
-// GLOBAL VARIABLES
-var toggleComplete = false;
-
 // EVENT LISTENERS
 // Click event listeners
 landingPage.addEventListener('click', function (event) {
+  showEasyMode(event);
+  showHardMode(event);
   displayEasyModePage(event);
   displayHardModePage(event);
 });
@@ -44,67 +47,64 @@ header.addEventListener('click', function (event) {
   displayHardModePage(event);
 });
 
+easyModeSection.addEventListener('click', function (event) {
+  selectChoice(event);
+});
+
 // Hover event listeners
-easyModeRock.addEventListener('mouseenter', function () {
-  showGradientSVG(easyRockIcons);
+easyModeRock.addEventListener('mouseenter', function (event) {
+  if (!event.target.classList.contains('choice-selected')) {
+    hideElement(easyRockWhiteIcon);
+    showElement(easyRockSVGIcon);
+  }
 });
-easyModeRock.addEventListener('mouseleave', function () {
-  hideGradientSVG(easyRockIcons);
-});
-
-easyModePaper.addEventListener('mouseenter', function () {
-  showGradientSVG(easyPaperIcons);
-});
-easyModePaper.addEventListener('mouseleave', function () {
-  hideGradientSVG(easyPaperIcons);
-});
-
-easyModeScissors.addEventListener('mouseenter', function () {
-  showGradientSVG(easyScissorsIcons);
-});
-easyModeScissors.addEventListener('mouseleave', function () {
-  hideGradientSVG(easyScissorsIcons);
+easyModeRock.addEventListener('mouseleave', function (event) {
+  if (!event.target.classList.contains('choice-selected')) {
+    showElement(easyRockWhiteIcon);
+    hideElement(easyRockSVGIcon);
+  }
 });
 
-hardModeRock.addEventListener('mouseenter', function () {
-  showGradientSVG(hardRockIcons);
+easyModePaper.addEventListener('mouseenter', function (event) {
+  if (!event.target.classList.contains('choice-selected')) {
+    hideElement(easyPaperWhiteIcon);
+    showElement(easyPaperSVGIcon);
+  }
 });
-hardModeRock.addEventListener('mouseleave', function () {
-  hideGradientSVG(hardRockIcons);
-});
-
-hardModePaper.addEventListener('mouseenter', function () {
-  showGradientSVG(hardPaperIcons);
-});
-hardModePaper.addEventListener('mouseleave', function () {
-  hideGradientSVG(hardPaperIcons);
-});
-
-hardModeScissors.addEventListener('mouseenter', function () {
-  showGradientSVG(hardScissorsIcons);
-});
-hardModeScissors.addEventListener('mouseleave', function () {
-  hideGradientSVG(hardScissorsIcons);
+easyModePaper.addEventListener('mouseleave', function (event) {
+  if (!event.target.classList.contains('choice-selected')) {
+    showElement(easyPaperWhiteIcon);
+    hideElement(easyPaperSVGIcon);
+  }
 });
 
-hardModeFire.addEventListener('mouseenter', function () {
-  showGradientSVG(hardFireIcons);
+easyModeScissors.addEventListener('mouseenter', function (event) {
+  if (!event.target.classList.contains('choice-selected')) {
+    hideElement(easyScissorsWhiteIcon);
+    showElement(easyScissorsSVGIcon);
+  }
 });
-hardModeFire.addEventListener('mouseleave', function () {
-  hideGradientSVG(hardFireIcons);
+easyModeScissors.addEventListener('mouseleave', function (event) {
+  if (!event.target.classList.contains('choice-selected')) {
+    showElement(easyScissorsWhiteIcon);
+    hideElement(easyScissorsSVGIcon);
+  }
 });
 
-hardModeWater.addEventListener('mouseenter', function () {
-  showGradientSVG(hardWaterIcons);
-});
-hardModeWater.addEventListener('mouseleave', function () {
-  hideGradientSVG(hardWaterIcons);
-});
+// hardModeRock.addEventListener('mouseenter', function () {});
+// hardModeRock.addEventListener('mouseleave', function () {});
 
-landingPage.addEventListener('click', function (event) {
-  showEasyMode(event);
-  showHardMode(event);
-});
+// hardModePaper.addEventListener('mouseenter', function () {});
+// hardModePaper.addEventListener('mouseleave', function () {});
+
+// hardModeScissors.addEventListener('mouseenter', function () {});
+// hardModeScissors.addEventListener('mouseleave', function () {});
+
+// hardModeFire.addEventListener('mouseenter', function () {});
+// hardModeFire.addEventListener('mouseleave', function () {});
+
+// hardModeWater.addEventListener('mouseenter', function () {});
+// hardModeWater.addEventListener('mouseleave', function () {});
 
 // FUNCTIONS
 // Helper functions
@@ -116,14 +116,14 @@ function hideElement(element) {
   element.classList.add('hidden');
 }
 
-// Dom Manipulation
+// Dom manipulation functions
 function displayEasyModePage(event) {
   if (event.target.classList.contains('easy-mode-button')) {
     showElement(easyModePage);
     showElement(header);
-    showElement(hardModeBtn)
+    showElement(hardModeBtn);
     hideElement(landingPage);
-    hideElement(easyModeBtn)
+    hideElement(easyModeBtn);
   }
 }
 
@@ -134,7 +134,7 @@ function displayHardModePage(event) {
     showElement(easyModeHeader);
     hideElement(landingPage);
     hideElement(hardModeHeader);
-    hideElement(easyModePage)
+    hideElement(easyModePage);
   }
 }
 
@@ -147,31 +147,68 @@ function hideHeader(event) {
   }
 }
 
-// Styling elements
-function showGradientSVG(icons) {
-  if (!toggleComplete) {
-    for (var i = 0; i < icons.length; i++) {
-      if (icons[i].classList.contains('hidden')) {
-        icons[i].classList.remove('hidden');
-      } else {
-        icons[i].classList.add('hidden');
-      }
-    }
+// Styling functions
+function selectChoice(event) {
+  displaySelectedChoice(event);
+  removeAllChoices(event);
+}
 
-    toggleComplete = true;
+function displaySelectedChoice(event) {
+  if (event.target.closest('section').classList.contains('choice')) {
+    var isChoiceElement = event.target.closest('.choice');
+    var allChoices = document.querySelectorAll('.choice');
+
+    if (isChoiceElement) {
+      for (let i = 0; i < allChoices.length; i++) {
+        allChoices[i].classList.remove('choice-selected');
+      }
+
+      if (isChoiceElement.classList.contains('rock')) {
+        showElement(easyRockSVGIcon);
+        showElement(easyPaperWhiteIcon);
+        showElement(easyScissorsWhiteIcon);
+        hideElement(easyPaperSVGIcon);
+        hideElement(easyScissorsSVGIcon);
+      } else if (isChoiceElement.classList.contains('paper')) {
+        showElement(easyPaperSVGIcon);
+        showElement(easyRockWhiteIcon);
+        showElement(easyScissorsWhiteIcon);
+        hideElement(easyRockSVGIcon);
+        hideElement(easyScissorsSVGIcon);
+      } else {
+        showElement(easyScissorsSVGIcon);
+        showElement(easyRockWhiteIcon);
+        showElement(easyPaperWhiteIcon);
+        hideElement(easyRockSVGIcon);
+        hideElement(easyPaperSVGIcon);
+      }
+
+      isChoiceElement.classList.add('choice-selected');
+    }
   }
 }
 
-function hideGradientSVG(icons) {
-  if (toggleComplete) {
-    for (var i = 0; i < icons.length; i++) {
-      if (icons[i].classList.contains('hidden')) {
-        icons[i].classList.remove('hidden');
-      } else {
-        icons[i].classList.add('hidden');
-      }
-    }
+function removeAllChoices(event) {
+  if (
+    !event.target.classList.contains('choice') &&
+    !event.target.classList.contains('options') &&
+    !event.target.classList.contains('play-btn')
+  )
+    resetOptions(event);
+}
 
-    toggleComplete = false;
+function resetOptions(event) {
+  var isChoiceElement = event.target.closest('.choice');
+  var allChoices = document.querySelectorAll('.choice');
+
+  for (let j = 0; j < allChoices.length; j++) {
+    allChoices[j].classList.remove('choice-selected');
   }
+
+  hideElement(easyRockSVGIcon);
+  hideElement(easyPaperSVGIcon);
+  hideElement(easyScissorsSVGIcon);
+  showElement(easyRockWhiteIcon);
+  showElement(easyPaperWhiteIcon);
+  showElement(easyScissorsWhiteIcon);
 }
